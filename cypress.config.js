e9930070cb76717a97801d2e0b549089d40f0fde
Diego <dmorenoo01@liverpool.com.mx>
2023-05-19 11:31:46 -0600

    gitactions prueba 11:29

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "f817jh",
+  //projectId: "f817jh",
   projectId: "mtg83j",
   e2e: {
     setupNodeEvents(on, config) {
